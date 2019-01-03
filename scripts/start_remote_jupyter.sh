517a328b219ee749e764b239db526a2f86d66e97
Thomas Kellermeier <t-kellermeier@hotmail.de>
2019-01-03 10:45:51 +0100

    add jupyger setup script

---
+++ scripts/start_remote_jupyter.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# trap ctrl-c and call ctrl_c()
+trap ctrl_c INT
+
+function ctrl_c() {
+        echo Jupyter notebook might be still running - process ID: $PID;
+        read -p "Do you want to kill the jupyter process? " yn
+                case $yn in
+                        [Yy]* ) echo "Killing process!"; kill $PID;;
+                        [Nn]* ) exit;;
+                esac
+}
+
+printf "\n\nStart Notebook\n" >> jupyter.logs;
+source venv/bin/activate;
+jupyter notebook &>> jupyter.logs &
+PID=$!;
+echo Jupyter process id: $PID >> jupyter.logs;
+tail -n 3 -f jupyter.logs;
+
+# Recommended: Create a notebook config for jupyter
+# Line 204 -> IP='0.0.0.0'
+# Line 276 -> Enter password after executing 'from notebook.auth import passwd; passwd()'
+# LIne 287 -> Port
